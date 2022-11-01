8f4eb4266c6e54af91ca284ea4cc6ae8f17c7919
rizwan-b <zjac077@live.rhul.ac.uk>
2022-11-01 23:50:57 +0000

    Deleted original NN .py

--- nearestNeighbour.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split #Import split method from Scikit-learn, this will allow us to split data sets into testing and training sets
-
