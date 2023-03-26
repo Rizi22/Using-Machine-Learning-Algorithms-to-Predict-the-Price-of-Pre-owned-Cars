ae3274260d4687892f9ba7f2fed634faaa573d44
rizwan-b <zjac077@live.rhul.ac.uk>
2023-03-26 15:30:20 +0100

    Updated linearRegression.py file to use updated code

--- linearRegression.py
+++ linearRegression.py
@@ -1,29 +1,28 @@
 import numpy as np 
 
-class linearRegression() :
-      
-    def __init__(self, learning_rate = 0.01, iterations = 1000) :
+class linearRegression():
+    def __init__(self,  learning_rate = 0.01, iterations = 1000):
         self.learning_rate = learning_rate
         self.iterations = iterations
-            
-    def fit(self, X, Y) :
-        self.sampleNumb, self.featuresNumb = X.shape 
-        self.weight = np.zeros(self.featuresNumb)  
-        self.bias = 0  
-        self.X = X 
-        self.Y = Y
-                  
-        for i in range(self.iterations) :             
-            self.weights()             
+        self.weights = None
+        
+    def fit(self, X_train, Y_train):
+        # Add a column of ones to X for the intercept term
+        X = np.insert(X_train, 0, 1, axis=1)
+        
+        # Initialize the weights to zero
+        self.weights = np.zeros(X.shape[1])
+        
+        # Performs gradient descent for the specified number of iterations
+        for i in range(self.iterations):
+            Y_pred = np.dot(X, self.weights)
+            error = Y_train - Y_pred
+            gradient = - (2 * (X.T).dot(error)) / X.shape[0]
+            self.weights = self.weights - self.learning_rate * gradient
+        
         return self
-      
-    def weights(self) :
-        Y_pred = self.predict(self.X)
-        weightGradient = - (2 * (self.X.T).dot(self.Y - Y_pred )) / self.sampleNumb
-        biasGradient = - 2 * np.sum(self.Y - Y_pred) / self.sampleNumb
-        self.weight = self.weight - self.learning_rate * weightGradient
-        self.bias = self.bias - self.learning_rate * biasGradient
-        return self
-      
-    def predict(self, X) :
-        return np.dot(X, self.weight) + self.bias
+        
+    def predict(self, X_test):
+        # Add a column of ones to X for the intercept term
+        X_test = np.insert(X_test, 0, 1, axis=1)
+        return np.dot(X_test, self.weights)
