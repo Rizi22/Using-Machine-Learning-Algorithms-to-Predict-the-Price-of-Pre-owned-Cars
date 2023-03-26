33603ed85322e179173cc97e42c19efd2dcf20ca
rizwan-b <zjac077@live.rhul.ac.uk>
2023-03-26 00:40:06 +0000

    Worked on combing the algorithms into one

--- UI_Interface.py
+++ UI_Interface.py
@@ -62,6 +62,12 @@ class mainMenuUI(QDialog):
                 Input_UI.setWindowTitle('LR Pre-Owned Car Price Predictor')
                 Input_UI.algorithm = "LR"
                 widget.setCurrentIndex(1)
+        
+        elif (self.combinedButton.isChecked()):
+                Input_UI.label1.setText("Combined ML Car Price Predictor")
+                Input_UI.setWindowTitle('Combined ML Pre-Owned Car Price Predictor')
+                Input_UI.algorithm = "combined"
+                widget.setCurrentIndex(1)
             
 
 class InputUI(QDialog):
@@ -97,6 +103,7 @@ class InputUI(QDialog):
 
         inputPred = []
 
+        print(self.chosenBrand(self.brandCombo.currentText()))
         X_train, X_test, Y_train, Y_test = self.loadDataset(self.chosenBrand(self.brandCombo.currentText()))   
         
         inputPred.append((self.modelEncoder.transform([self.modelCombo.currentText()]))[0])
@@ -130,6 +137,33 @@ class InputUI(QDialog):
                 RF = randomForest()
                 RF.fit(X_train, Y_train)
                 Y_pred = RF.predict([inputPred])
+            
+            case "combined":
+                X_train, X_test, Y_train, Y_test, scaled_X_train, scaled_X_test, scaled_Y_train, scaled_Y_test = self.loadDataset(self.chosenBrand(self.brandCombo.currentText())) 
+                
+                print("GOT HEREE 1")
+                DT = decisionTree(3, 93)
+                DT.fit(X_train, Y_train)
+                Y_pred1 = DT.predict([inputPred])
+
+                print("GOT HEREE 2")
+                RF = randomForest()
+                RF.fit(X_train, Y_train)
+                Y_pred2 = RF.predict([inputPred])
+
+                inputPred = self.scaler.transform([inputPred])
+
+                LR = linearRegression()
+                LR.fit(scaled_X_train, scaled_Y_train)
+                Y_pred3 = LR.predict(inputPred)
+                
+                KNN = nearestNeighbour()
+                Y_pred4 = KNN.predict(scaled_X_train, inputPred, scaled_Y_train, 4)
+                
+                print(Y_pred1, Y_pred2, Y_pred3, Y_pred4)
+                Y_pred = (Y_pred1 + Y_pred2 + Y_pred3 + Y_pred4) / 4
+                
+
 
         print("\n Predicted price for your car is: £", round(Y_pred[0], 2))
         print("\n ***Predicted in", time.time() - timer,"seconds***")
@@ -204,6 +238,20 @@ class InputUI(QDialog):
                 X_train = self.scaler.transform(X_train)
                 X_test = self.scaler.transform(X_test)
                 return  X_train, X_test, Y_train, Y_test
+            
+            case "combined":
+                X = file.drop(['price'], axis = 1).to_numpy()
+                Y = file['price'].values.reshape(-1,1)
+                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 601)
+
+                X = file.drop(columns = ['price'])
+                Y = file.price
+                scaled_X_train, scaled_X_test, scaled_Y_train, scaled_Y_test = train_test_split(X, Y, random_state = 601)
+                self.scaler.fit(scaled_X_train)
+                scaled_X_train = self.scaler.transform(scaled_X_train)
+                scaled_X_test = self.scaler.transform(scaled_X_test)
+                
+                return  X_train, X_test, Y_train, Y_test, scaled_X_train, scaled_X_test, scaled_Y_train, scaled_Y_test
         
 
     def addModel(self):
@@ -233,8 +281,10 @@ class predPage(QDialog):
         Input_UI.pred.clear()
         Input_UI.pred2.clear()
         Input_UI.yearBox.clear()
-        for i in reversed(range(layout.count())): 
-            layout.itemAt(i).widget().setParent(None)
+        Input_UI.mileageBox.clear()
+        Input_UI.taxBox.clear()
+        Input_UI.mpgBox.clear()
+        Input_UI.engineBox.clear()
 
     def clearText(self):
         Input_UI.pred.clear()
